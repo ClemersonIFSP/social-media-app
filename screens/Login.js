4840062c326b18038b1e6e4b2724cbca5ae3943f
Clemerson Santos <clemerson.santos@aluno.ifsp.edu.br>
2024-05-14 12:40:46 -0300

    Add background image to login screen and css

--- screens/Login.js
+++ screens/Login.js
@@ -1,42 +1,55 @@
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import { View, Text, StyleSheet, TextInput, Button, ImageBackground } from "react-native";
+const image = {}
 export const Login = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.loginContainer}>
-        <Text style={{ marginBottom: 10 }}>Login</Text>
-        <View>
-          <Text>E-mail</Text>
+    <ImageBackground source={require("../assets/background.jpg")} style={styles.container}>
+      <View style={styles.formContainer}>
+        <Text style={styles.h1}>LOGIN</Text>
+        <View style={styles.form} >
+          <Text style={styles.text}>E-mail:</Text>
           <TextInput style={styles.input}></TextInput>
         </View>
-        <View>
-          <Text>Senha</Text>
-          <TextInput style={styles.input}></TextInput>
+        <View style={styles.form}>
+          <Text style={styles.text}>Senha:</Text>
+          <TextInput style={styles.input} secureTextEntry={true}></TextInput>
         </View>
         <View style={styles.btnGroup}>
-          <Button title="Register" />
+          <Button title="Registrar" />
           <Button title="Logar" />
         </View>
       </View>
-    </View>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
     justifyContent: "center",
   },
-  loginContainer: {
-    width: 400,
-    height: 400,
+  formContainer: {
     alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#fff",
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    margin: 20
+  },
+  text: {
     
   },
+  h1: {
+    fontSize: 25,
+    fontWeight: "700",
+  },
+  form: {
+    width: "100%",
+  },
   input: {
-    borderWidth: 1,
+    borderBottomWidth: 1,
+    backgroundColor: "#f9f9f9",
+    borderRadius: 5,
+    marginBottom: 10,
+    padding: 5,
   },
   btnGroup: {
     flexDirection: "row",
